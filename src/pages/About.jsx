9845de63bdc528e803da41fefcd4fca076849783
uguryilmaz.dev <uguryilmaz.dev@gmail.com>
2023-10-14 20:37:28 +0300

    Add About page content and style

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,29 @@
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+import chefPhoto1 from '../assets/chef1.jpg'
+import chefPhoto2 from '../assets/chef2.jpg'
+import '../styles/About.css'
+
 export default function About() {
     return (
         <>
-            this is about
+            <Nav />
+            <main id="page-about-container">
+                <h1>Hey there! We are George and Bob</h1>
+                <div>
+                    <img
+                    src={chefPhoto1}
+                    alt="The owners of restaurant are in the kitchen"
+                    className="about-image"
+                    />
+                    <img
+                    src={chefPhoto2}
+                    alt="The owners of restaurant are in the kitchen"
+                    className="about-image"
+                    />
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
